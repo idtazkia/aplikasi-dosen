c33f314fdaa8490f215b7f901c5b73c7f8639595
Addinul Kintangi <addins3009@gmail.com>
2017-04-14 23:56:32 +0700

    implementasi crud jenisSurat

--- src/main/java/id/ac/tazkia/dosen/controller/JenisSuratController.java
+++ src/main/java/id/ac/tazkia/dosen/controller/JenisSuratController.java
@@ -3,23 +3,61 @@ package id.ac.tazkia.dosen.controller;
 /**
  * Created by yogi on 03/04/2017.
  */
+
+import id.ac.tazkia.dosen.dao.JenisSuratDao;
+import id.ac.tazkia.dosen.entity.JenisSurat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.support.SessionStatus;
 
-import java.util.Date;
+import javax.validation.Valid;
 
 @Controller
 public class JenisSuratController {
 
-    @RequestMapping("/jenissurat/list")
-    public ModelMap jenissurat(){
-        return new ModelMap();
+    private JenisSuratDao jenisSuratDao;
+
+    public JenisSuratController(JenisSuratDao jenisSuratDao) {
+        this.jenisSuratDao = jenisSuratDao;
     }
 
-    @RequestMapping(value = "/jenissurat/form", method = RequestMethod.GET)
-    public void tampilkanForm() {
+    @GetMapping("/jenissurat/list")
+    public ModelMap jenissurat(){
+        return new ModelMap().addAttribute("jenisSurat", jenisSuratDao.findAll());
+    }
+
+    @GetMapping("/jenissurat/form")
+    public ModelMap tampilkanForm(@RequestParam(value = "id", required = false) JenisSurat jenisSurat) {
+        if (jenisSurat == null) {
+            jenisSurat = new JenisSurat();
+        }
+        return new ModelMap("jenisSurat", jenisSurat);
+    }
+
+    @PostMapping("/jenissurat/form")
+    public String simpan(@ModelAttribute @Valid JenisSurat jenisSurat, BindingResult err, SessionStatus status) {
+        if (err.hasErrors()) {
+            return "/jenissurat/form";
+        }
+        jenisSuratDao.save(jenisSurat);
+        status.setComplete();
+        return "redirect:/jenissurat/list";
+    }
+
+    @GetMapping("/jenissurat/delete")
+    public ModelMap deleteConfirm(@RequestParam(value = "id", required = true) JenisSurat jenisSurat) {
+        return new ModelMap("jenisSurat", jenisSurat);
+    }
+
+    @PostMapping("/jenissurat/delete")
+    public String delete(@ModelAttribute JenisSurat jenisSurat, SessionStatus status) {
+        jenisSuratDao.delete(jenisSurat);
+        status.setComplete();
+        return "redirect:/jenissurat/list";
     }
 }
