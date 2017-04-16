63eaefa197843a8dddbbf31f460584ba477851c8
Endy Muhardin <endy.muhardin@gmail.com>
2017-04-16 19:33:55 +0700

    baseurl tarik ke base class

---
+++ src/test/java/id/ac/tazkia/dosen/controller/BaseSeleniumTests.java
@@ -0,0 +1,25 @@
+package id.ac.tazkia.dosen.controller;
+
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.PostConstruct;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public abstract class BaseSeleniumTests {
+
+    @LocalServerPort
+    private int serverPort;
+
+    protected String baseUrl;
+
+    @PostConstruct
+    public void setupWebDriver(){
+        baseUrl = String.format("http://localhost:%d", serverPort);
+    }
+}
--- src/test/java/id/ac/tazkia/dosen/controller/SuratTugasTests.java
+++ src/test/java/id/ac/tazkia/dosen/controller/SuratTugasTests.java
@@ -2,40 +2,29 @@ package id.ac.tazkia.dosen.controller;
 
 import com.github.javafaker.Faker;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-import org.springframework.boot.context.embedded.LocalServerPort;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.text.SimpleDateFormat;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class SuratTugasTests {
+public class SuratTugasTests extends BaseSeleniumTests {
 
     private WebDriver webDriver = new HtmlUnitDriver();
-
-    @LocalServerPort
-    private int serverPort;
 
     private Faker faker = new Faker();
     private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
     @Test
     public void testListView() throws Exception {
-        String baseUrl = String.format("http://localhost:%d", serverPort);
         webDriver.get(baseUrl + "/surattugas/list");
         assertThat(webDriver.getTitle()).isEqualTo("Aplikasi Dosen :: Surat Tugas");
     }
 
     @Test
     public void testSubmitFormGagal() throws Exception {
-        String baseUrl = String.format("http://localhost:%d", serverPort);
         webDriver.get(baseUrl + "/surattugas/form");
         assertThat(webDriver.getTitle()).isEqualTo("Aplikasi Dosen :: Edit Surat Tugas");
 
