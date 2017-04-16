ff0343600e7a197ad06e7495ce15bb519877f575
haikal <haikal00@gmail.com>
2017-04-16 23:09:17 +0800

    update schema + data : Jenis Surat

--- src/main/resources/db/migration/V1.0.0.3__Data_Awal.sql
+++ src/main/resources/db/migration/V1.0.0.3__Data_Awal.sql
@@ -1,8 +1,8 @@
 INSERT INTO `dosen` VALUES ('5d9b24ff-21d9-44d6-8781-d6b5254c6bd2', 'Dosen 1');
 INSERT INTO `dosen` VALUES ('47a1b8e8-fc1f-4082-8dea-6fa7a4899bbd', 'Dosen 2');
 
-INSERT INTO `jenis_surat` VALUES ('6a20627f-65db-4a40-b92f-c575daaef370', 'Jenis Surat 1');
-INSERT INTO `jenis_surat` VALUES ('af6d73bb-cf8e-4c47-be1c-9a93bc1f1e21', 'Jenis Surat 2');
+INSERT INTO `jenis_surat` VALUES ('6a20627f-65db-4a40-b92f-c575daaef370', 'Jenis Surat 1', 'Sampel nomer 1', 'belum ada template');
+INSERT INTO `jenis_surat` VALUES ('af6d73bb-cf8e-4c47-be1c-9a93bc1f1e21', 'Jenis Surat 2', 'Sampel nomer 2', 'belum ada template juga');
 
 INSERT INTO `surat_tugas` VALUES ('4fd3cf54-77dc-4523-9bab-36387987d99e', '123', '6a20627f-65db-4a40-b92f-c575daaef370',
                                   '5d9b24ff-21d9-44d6-8781-d6b5254c6bd2', '2017-04-14', '2017-04-15');
