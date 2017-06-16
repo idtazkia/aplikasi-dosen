67abe0648ee0f7e5feb7b47f47161e3fbfed8da7
razisyahputro <razisyahputro@gmail.comgit config --global user.email razisyahputro@gmail.comgit config --global user.email razisyahputro@gmail.com>
2017-06-16 16:41:51 +0700

    tambah poin kegiatan

--- src/main/java/id/ac/tazkia/dosen/controller/PoinKegiatanController.java
+++ src/main/java/id/ac/tazkia/dosen/controller/PoinKegiatanController.java
@@ -3,51 +3,104 @@ package id.ac.tazkia.dosen.controller;
 import id.ac.tazkia.dosen.dao.JabatanDao;
 import id.ac.tazkia.dosen.dao.JenisKegiatanDao;
 import id.ac.tazkia.dosen.dao.JenisSuratDao;
+import id.ac.tazkia.dosen.dao.PoinKegiatanDao;
 import id.ac.tazkia.dosen.entity.*;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PoinKegiatanController {
 
     @Autowired
     private JabatanDao jabatanDao;
+    
+    @Autowired
+    private PoinKegiatanDao poinKegiatanDao;
 
     @Autowired
     private JenisKegiatanDao jenisKegiatanDao;
-
-    @ModelAttribute("daftarJabatan")
-    public Iterable<Jabatan> daftarJabatan() {
-        return jabatanDao.findAll();
+    
+    @RequestMapping("/poinkegiatan/list")
+    public String poinKegiatan(Model model) {
+        model.addAttribute("poinKegiatanList", poinKegiatanDao.findAll());
+        return "/poinkegiatan/list";
     }
-
-    @ModelAttribute("daftaJenisKegiatan")
-    public Iterable<JenisKegiatan> daftarJenisKegiatan() {
-        return jenisKegiatanDao.findAll();
-    }
-
-    @GetMapping("/poinkegiatan/list")
-    public void list(){
-
-    }
-
+    
     @GetMapping("/poinkegiatan/form")
-    public ModelMap tampilkanForm(@RequestParam(value = "id", required = false) PoinKegiatan poinKegiatan) {
+    public String tampilkanForm(@RequestParam(value = "id", required = false) PoinKegiatan poinKegiatan, Model m) {
         if (poinKegiatan == null) {
             poinKegiatan = new PoinKegiatan();
         }
-        return new ModelMap("poinKegiatan", poinKegiatan);
+        m.addAttribute("poinKegiatan", poinKegiatan);
+        m.addAttribute("listJabatan", jabatanDao.findAll());
+        m.addAttribute("listJenisKegiatan", jenisKegiatanDao.findAll());
+        return "/poinkegiatan/form";
     }
-
-
-
+    
     @PostMapping("/poinkegiatan/form")
-    public String prosesForm(){
+    public String simpan(@ModelAttribute @Valid PoinKegiatan poinKegiatan, BindingResult err, SessionStatus status) {
+        if (err.hasErrors()) {
+            return "/poinkegiatan/form";
+        }
+        poinKegiatanDao.save(poinKegiatan);
+        status.setComplete();
         return "redirect:/poinkegiatan/list";
     }
+    
+    @GetMapping("/poinkegiatan/delete")
+    public ModelMap deleteConfirm(@RequestParam(value = "id", required = true) PoinKegiatan poinkegiatan) {
+        return new ModelMap("poinkegiatan", poinkegiatan);
+    }
+
+    @PostMapping("/poinkegiatan/delete")
+    public Object delete(@ModelAttribute PoinKegiatan poinkegiatan, SessionStatus status) {
+        try {
+            poinKegiatanDao.delete(poinkegiatan);
+        } catch (DataIntegrityViolationException exception) {
+            status.setComplete();
+            return new ModelAndView("error/errorHapus")
+                    .addObject("entityId", poinkegiatan.getId())
+                    .addObject("entityName", "PoinKegiatan")
+                    .addObject("errorCause", exception.getRootCause().getMessage())
+                    .addObject("backLink", "/poinkegiatan/list");
+        }
+        status.setComplete();
+        return "redirect:/poinkegiatan/list";
+    }
+    
+//    @ModelAttribute("daftarJabatan")
+//    public Iterable<Jabatan> daftarJabatan() {
+//        return jabatanDao.findAll();
+//    }
+//
+//    @ModelAttribute("daftaJenisKegiatan")
+//    public Iterable<JenisKegiatan> daftarJenisKegiatan() {
+//        return jenisKegiatanDao.findAll();
+//    }
+//
+//    @GetMapping("/poinkegiatan/list")
+//    public void list(){
+//
+//    }
+//
+    
+//
+//
+//
+//    @PostMapping("/poinkegiatan/form")
+//    public String prosesForm(){
+//        return "redirect:/poinkegiatan/list";
+//    }
 }
