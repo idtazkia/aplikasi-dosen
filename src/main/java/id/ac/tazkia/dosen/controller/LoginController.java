41930a7a7a3929aa8d5ba8347470640fc55d0869
ivans ardiansyah <vaansaa@gmail.com>
2017-06-16 10:08:46 +0700

    implement login page

---
+++ src/main/java/id/ac/tazkia/dosen/controller/LoginController.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.ac.tazkia.dosen.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ * @author ivans
+ */
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String getlogin() {
+        return "login";
+    }
+}
