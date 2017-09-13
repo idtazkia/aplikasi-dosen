f6bbb21a59b814f76ec7fdc69272f3a12e3e8f05
arifeb24 <ari.febri24@gmail.com>
2017-09-13 11:48:15 +0700

    menambahkan field fakultas pada menu data master program studi

--- src/main/java/id/ac/tazkia/dosen/controller/ProgramStudiController.java
+++ src/main/java/id/ac/tazkia/dosen/controller/ProgramStudiController.java
@@ -1,14 +1,21 @@
 package id.ac.tazkia.dosen.controller;
 
+import id.ac.tazkia.dosen.dao.FakultasDao;
 import id.ac.tazkia.dosen.dao.ProgramStudiDao;
+import id.ac.tazkia.dosen.entity.Fakultas;
 import id.ac.tazkia.dosen.entity.ProgramStudi;
+import java.util.ArrayList;
+import java.util.List;
 import javax.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +28,10 @@ public class ProgramStudiController {
 
     @Autowired
     private ProgramStudiDao psd;
+    @Autowired
+    private FakultasDao fakultasDao;
+
+    private static Logger LOGGER = LoggerFactory.getLogger(ProgramStudiController.class);
 
     @RequestMapping("/programstudi/list")
     public void daftarProgramStudi(@PageableDefault(size = 10) Pageable pageable, @RequestParam(name = "value", required = false) String value, Model model) {
@@ -44,6 +55,16 @@ public class ProgramStudiController {
 
         ProgramStudi programStudi = new ProgramStudi();
         m.addAttribute("programStudi", programStudi);
+        List<Fakultas> listFakultas = new ArrayList<>();
+
+        if (fakultasDao.count() > 0) {
+            Pageable pageable = new PageRequest(0, (int) fakultasDao.count());
+            Page<Fakultas> pageFakultas = fakultasDao.findAll(pageable);
+            listFakultas = pageFakultas.getContent();
+            LOGGER.info("ini id fakultas [{}]", listFakultas.get(1).getId());
+        }
+        LOGGER.info("ini id fakultas [{}]", listFakultas.get(1).getId());
+        m.addAttribute("fakultass", listFakultas);
 
         if (id != null && !id.isEmpty()) {
             programStudi = psd.findOne(id);
@@ -54,16 +75,24 @@ public class ProgramStudiController {
                 return "programstudi/form";
             }
         }
+
         return "programstudi/form";
     }
 
     @RequestMapping(value = "/programstudi/form", method = RequestMethod.POST)
-    public String prosesForm (@ModelAttribute @Valid ProgramStudi programStudi, BindingResult err, SessionStatus status) {
+    public String prosesForm(@ModelAttribute @Valid ProgramStudi programStudi, BindingResult err, SessionStatus status, Model model) {
         if (err.hasErrors()) {
+            List<Fakultas> listFakultas = new ArrayList<>();
+            if (fakultasDao.count() > 0) {
+                Pageable pageable = new PageRequest(0, (int) fakultasDao.count());
+                Page<Fakultas> pageFakultas = fakultasDao.findAll(pageable);
+                listFakultas = pageFakultas.getContent();
+            }
+            model.addAttribute("fakultass", listFakultas);
             return "/programstudi/form";
         }
         psd.save(programStudi);
         status.setComplete();
         return "redirect:/programstudi/list";
     }
-    }
+}
